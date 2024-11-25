21574dec8782db26b8a240972cabba3844a5ecf2
til <m15071452418@163.com>
2024-11-25 14:10:35 +0800

    0

---
+++ src/main/java/com/til/wtcr_service/service/ArticlesService.java
@@ -0,0 +1,13 @@
+package com.til.wtcr_service.service;
+
+import com.til.wtcr_service.pojo.Articles;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author til
+* @description 针对表【articles】的数据库操作Service
+* @createDate 2024-11-25 14:06:13
+*/
+public interface ArticlesService extends IService<Articles> {
+
+}
---
+++ src/main/wtcr_service/mapper/ArticlesMapper.java
@@ -0,0 +1,18 @@
+package java.com.til.wtcr_service.mapper;
+
+import com.til.wtcr_service.pojo.Articles;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author til
+* @description 针对表【articles】的数据库操作Mapper
+* @createDate 2024-11-25 14:03:49
+* @Entity com.til.wtcr_service.pojo.Articles
+*/
+public interface ArticlesMapper extends BaseMapper<Articles> {
+
+}
+
+
+
+
